38d6c0e9383adea88d893ac06a8fc9c9c36adbd0
Gauravmazumdar elitemindz <gauravmazumdarEM>
2025-04-10 18:19:02 +0530

    _Code_merged_together_

--- chat_app_frontend/src/component/HomeComponent/HomeComponent.jsx
+++ chat_app_frontend/src/component/HomeComponent/HomeComponent.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Leftside from '../FormatComponents/LeftSide/Leftside';
+import Rightside from '../FormatComponents/RightSide/Rightside';
+
+
 
 function HomeComponent() {
   return (
-    <div>HomeComponent</div>
+    <div className="container-fluid">
+    {/* This is the ball that will follow the cursor */}
+    <div className="ball"></div>
+
+    <div className="row vh-100">
+      <div className="col-12 col-sm-3 left_side_styles">
+        <Leftside />
+      </div>
+      <div className="col-12 col-sm-9 right_side_styles">
+        <Rightside />
+      </div>
+    </div>
+  </div>
   )
 }
 
