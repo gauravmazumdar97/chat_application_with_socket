9df1a84150e4e6d519da92ed965d2ceea183bd6d
Gauravmazumdar elitemindz <gauravmazumdarEM>
2025-05-05 10:49:14 +0530

    API_Changes

--- chat_app_frontend/Interceptor/Inteceptor.js
+++ chat_app_frontend/Interceptor/Inteceptor.js
@@ -1,42 +1,4 @@
 
-// import axios from 'axios';
-// import { environment } from '../environment'; 
-
-// const Interceptor = axios.create({
-//   baseURL: `${environment.serverUrl}${environment.userApi}`,
-//   headers: { 'Content-Type': 'application/json' },
-// });
-
-// // 👉 Request Interceptor: add token
-// Interceptor.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token'); // or sessionStorage
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// Interceptor.interceptors.response.use(
-//   (response) => response.data ? response.data : response,
-//   (error) => {
-//     if (error.response) {
-//       console.error('API Error:', error.response.data.message);
-//       // Optionally redirect to login or show toast
-//     } else {
-//       console.error('Network Error:', error.message);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default Interceptor;
-
-
-
-
 import axios from 'axios';
 import { environment } from '../environment';
 
