0099cba610bc8dcb920e9ab7dbe2b2657723f833
Gaurav Mazumdar <shehenshaahind@gmail.com>
2025-05-12 07:48:27 +0530

    Latest cpde

--- chat_app_frontend/environment.js
+++ chat_app_frontend/environment.js
@@ -2,7 +2,7 @@ export const environment = {
     userApi: '/api/users', // Gaurav
     authApi: '/api/auth', // Gaurav
     // serverUrl: 'http://192.168.1.9:7000', // Gaurav
-    serverUrl: 'http://192.168.20.227:7000', // Gaurav
+    serverUrl: 'http:// 192.168.1.2:7000', // Gaurav
   
     // fileUrl: 'http://192.168.20.245:5000/uploadDoc/',
     fileUrl: 'https://mdms.api.elitetraveltech.in/uploadDoc/',
